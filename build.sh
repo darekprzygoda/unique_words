5b00c4577331a054182a934c28aa41dfe11e1dbd
Darek Przygoda <darekp@alatek.krakow.pl>
2024-05-03 12:36:13 +0200

    set custom path to Catch2 in cmake command line using -DCATCH2_DIR

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-cmake -DCMAKE_BUILD_TYPE=DEBUG -S . -B build/debug -G Ninja
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CATCH2_DIR=${SCRIPT_DIR}/../../Catch2/deploy
+
+cmake -DCMAKE_BUILD_TYPE=DEBUG -S . -B build/debug -G Ninja -DCATCH2_DIR=${CATCH2_DIR}
 cmake --build build/debug --verbose
 
-cmake -DCMAKE_BUILD_TYPE=Release -S . -B build/release -G Ninja
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B build/release -G Ninja -DCATCH2_DIR=${CATCH2_DIR}
 cmake --build build/release --verbose
 
